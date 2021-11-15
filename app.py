181797ad6a593d14727deb40d5a41c0f711c00e0
tahasarnic <40581446+tahasarnic@users.noreply.github.com>
2021-11-15 22:47:15 +0300

    Add files via upload

---
+++ app.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Nov 15 21:20:33 2021
+
+@author: terens
+"""
+
+import streamlit as st
+import pandas as pd
+import numpy as np
+import random
+import os
+
+st.set_page_config(layout = "wide", initial_sidebar_state = "collapsed")
+
+# Başlık
+st.title("Egenaz'ın Yapay Zekasına Hoşgeldiniz")
+
+# İstek seçtir
+secenek = st.radio("Ne isterseniz Egenaz hanım?", options = ['Mimik yapsın', 'Sohbet etsin', 'Sırtımı kaşısın'])
+
+if secenek == 'Mimik yapsın':
+    if st.button('Mimik Değiştir 🤗'):
+        mimikler = os.listdir("/App/img")
+        mimikler.remove('.DS_Store')
+        st.image("/App/img/" + str(random.choice(mimikler)))
+        
+if secenek == "Sohbet etsin":
+    if st.button('Konuş 👦'):
+        konus = ['Selaaam 🙋🏻', 'Günün nasıl geçti? 💁🏻‍♂️', 'Kaç çocuk mıncırdın bugün? 👶🏻',
+                 'Su içmeyi unutma! 💧', 'Bu çocukla görüşmen nasıl geçti? 😉', 'Bence iyi bir çocuğa benziyor 😛',
+                 'Turuncu sana çok yakışıyor 🥕']
+        st.write(random.choice(konus))
+        
+if secenek == "Sırtımı kaşısın":
+    if st.button('Sırtımı kaşı 🤤'):
+        st.image("/App/kasi.png")
+        
+        
