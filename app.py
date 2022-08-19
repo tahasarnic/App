dca30733eb2882abffb5ce07773e96d37c7bdcbd
Taha Eren SARNIÇ <terens@Tahas-MacBook-Air.local>
2022-08-20 01:02:28 +0300

    güncellendi

--- app.py
+++ app.py
@@ -15,45 +15,99 @@ import os
 st.set_page_config(layout = "wide", initial_sidebar_state = "collapsed")
 
 # Başlık
-st.title("Egenaz'ın Yapay Zekasına Hoşgeldiniz")
+st.title("Egenaz'ın Yapay Zekasına Hoşgeldiniz Vol 2")
+
+st.markdown("Hadi seninle bir güven testi yapalım. Bu aplikasyonun sonuna gelene kadar **SOL** tarafa bakmak yok. Ne zaman **'Şimdi SOL tarafa bakabilirsin'** ifadesiyle karşılaşırsan işte o zaman sol tarafa bakabilirsin. Bu testten geçeceğine %100 eminim.")
+
+sola_bakmak = st.radio('Sola bakmama kararı veriniz:', options = ['Söz söz valla bakmayacağım 😇', 'Bir gözüm kayabilir 😈'], index = 1)
+if sola_bakmak == 'Bir gözüm kayabilir 😈':
+    st.image('img/ordek.jpeg')
+else:
+    st.balloons()
+    st.image('img/tebrik.png')
+
+st.markdown('Belki bu aplikasyona bakarken seni bana, muhtemelen beni de sana hatırlatan şarkımızı dinlemek istersin 👇')
+
+if st.button('Şarkı Çalsın 🎶'):
+    st.video('music/Kaan Boşnak - Seni Buldum Ya.mp4', format='video/mp4')
+
+
+st.markdown('Evet bu hikayenin konusu da aslında **"seni buldum ya başka ne isterim?"** Gel beraber bu hikayeye bir göz atalım 👀')
+
+st.markdown('## Bizim Kumarımız Burada Biter')
+
+col1, col2 = st.columns([0.3, 1])
+
+with col1:
+    hikaye = st.radio('Hikaye seçiniz:', ['Toz Bulutu', 'Haller', 'Kapılar', 'Kazandibi', 'Pokemon', 'Mor', 'Premses', 'NASA', 'Denge', 'Güneş'])
+    hikaye_dict = {'Toz Bulutu':'1',
+                   'Haller':'2',
+                   'Kapılar':'3',
+                   'Kazandibi':'4',
+                   'Pokemon':'5',
+                   'Mor':'6',
+                   'Premses':'7',
+                   'NASA':'8',
+                   'Denge':'9',
+                   'Güneş':'10'}
+
+with col2:
+    st.image('img1/'+hikaye_dict[hikaye]+'.png')
+
+st.markdown('## Sola Bakmak?')
+st.markdown('Tebrikler Ege Naz Bozovalı testi geçtin artık **SOLA BAKABİLİRSİN** 👇')
+st.image('img1/20220820_004117.jpg')
+
+cevap = st.radio('Cevabınız?', ['Eveeeeeeeet', 'Nau Nau'])
+
+if cevap == 'Eveeeeeeeet':
+    st.image('img1/11.png')
+else:
+    st.image('img1/12.png')
+
+
+    
+
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 # İstek seçtir
-secenek = st.radio("Ne isterseniz Egenaz hanım?", options = ['Mimik yapsın', 'Sohbet etsin', 'Sırtımı kaşısın'])
+#secenek = st.radio("Ne isterseniz Egenaz hanım?", options = ['Mimik yapsın', 'Sohbet etsin', 'Sırtımı kaşısın'])
 
-if secenek == 'Mimik yapsın':
-    if st.button('Mimik Değiştir 🤗'):
-        mimikler = ["img/1.jpeg", "img/2.png", "img/3.jpeg", "img/4.webp", "img/5.jpeg", "img/6.png", "img/7.jpeg", "img/8.jpeg", "img/9.jpeg", "img/10.jpeg"]
-        st.image(random.choice(mimikler))
+#if secenek == 'Mimik yapsın':
+#    if st.button('Mimik Değiştir 🤗'):
+#        mimikler = ["img/1.jpeg", "img/2.png", "img/3.jpeg", "img/4.webp", "img/5.jpeg", "img/6.png", "img/7.jpeg", "img/8.jpeg", "img/9.jpeg", "img/10.jpeg"]
+#        st.image(random.choice(mimikler))
         
-if secenek == "Sohbet etsin":
-    if st.button('Konuş 👦'):
-        konus = ['Selaaam 🙋🏻', 'Günün nasıl geçti? 💁🏻‍♂️', 'Kaç çocuk mıncırdın bugün? 👶🏻',
-                 'Su içmeyi unutma! 💧', 'Bu çocukla görüşmen nasıl geçti? 😉', 'Bence iyi bir çocuğa benziyor 😛',
-                 'Turuncu sana çok yakışıyor 🥕']
-        st.write(random.choice(konus))
+#if secenek == "Sohbet etsin":
+#    if st.button('Konuş 👦'):
+#        konus = ['Selaaam 🙋🏻', 'Günün nasıl geçti? 💁🏻‍♂️', 'Kaç çocuk mıncırdın bugün? 👶🏻',
+#                 'Su içmeyi unutma! 💧', 'Bu çocukla görüşmen nasıl geçti? 😉', 'Bence iyi bir çocuğa benziyor 😛',
+#                 'Turuncu sana çok yakışıyor 🥕']
+#        st.write(random.choice(konus))
         
-if secenek == "Sırtımı kaşısın":
-    if st.button('Sırtımı kaşı 🤤'):
-        st.image("kasi.png")
+#if secenek == "Sırtımı kaşısın":
+#    if st.button('Sırtımı kaşı 🤤'):
+#        st.image("kasi.png")
 
-#col1, col2, col3 = st.columns([0.2,1,0.2])
-#with col2:
-#    st.title('24 Kasım Öğretmenler Günün Kutlu Olsun Egoş')
-#    st.balloons()
-#    st.image('img/cocuklar_egos.png')   
-    
-#with st.expander("Peki kim mi bu Egenaz?👩🏻‍🦰"):
-#    col7, col8, col9 = st.columns([0.6,1,0.2])
-#    with col8:
-#        st.title('Öncelikle kendisi...')
-#        st.image('img/oscar.jpeg', width = 400)
-        
-#with st.expander("Yaa çok merak ettim, biraz daha bahseder misin?😯 (ve Eren'in mektubu başlar)"):
-#    col4, col5, col6 = st.columns([0.3,1,0.2])
-#    with col5:
-#        st.image('img/mektup.png')
-        
 
-#with st.expander("Egenaz'ın Beyaz Duvarı 🖼🎭"):
-#    st.image('img/duvar.png')
-        
